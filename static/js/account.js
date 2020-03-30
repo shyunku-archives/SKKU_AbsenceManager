feae3fbb2e0270f9427818dffef778e83570caac
whdudgns412 <whdudgns7321@gmail.com>
2020-03-30 15:00:36 +0900

    display timetable candidates info remove focusing visual effect

--- static/js/account.js
+++ static/js/account.js
@@ -9,6 +9,7 @@ function fetchTimeTableInfo(){
     const fetchTimeTableLoadingBar = $('#load_timetable_list_bar');
     const timetableDecisionWrapper = $('#timetable_decision_wrapper');
     const timetableCandidateSelector = $('#timetable_candidate_selector');
+    $('#verify_account_btn').attr('disabled', true);
     activateLoadingBar(fetchTimeTableLoadingBar);
     const id = $('#everytime_id_input').val();
     const pw = $('#everytime_pw_input').val();
@@ -22,12 +23,29 @@ function fetchTimeTableInfo(){
         },
         success: function(res){
             const tables = res.tables;
+            console.log(tables);
             deactivateLoadingBar(fetchTimeTableLoadingBar);
             deactiveBlurredDiv(timetableDecisionWrapper);
             for(let i=0;i<tables.length;i++){
                 let tableInfo = tables[i];
                 timetableCandidateSelector.append(new Option(tableInfo.name, tableInfo.id));
             }
+            $('#selected_subject_id').text(tables[0].id);
+            $('#selected_subject_recent_update_date').text(tables[0].update_date);
+            $('#selected_subject_generated_date').text(tables[0].create_date);
+
+            timetableCandidateSelector.on('change', () => {
+                let selectedValue = $('#timetable_candidate_selector option:selected').val();
+                for(let i=0;i<tables.length;i++){
+                    let tableInfo = tables[i];
+                    if(tableInfo.id == selectedValue){
+                        $('#selected_subject_id').text(tableInfo.id);
+                        $('#selected_subject_recent_update_date').text(tableInfo.update_date);
+                        $('#selected_subject_generated_date').text(tableInfo.create_date);
+                        return;
+                    }
+                }
+            });
         }
     });
 }
