581542a8140be5826f0b2e4977eb982d5a5a2509
whdudgns412 <whdudgns7321@gmail.com>
2020-04-08 04:54:23 +0900

    end left-sidebar add development info

--- static/js/sidemenu.js
+++ static/js/sidemenu.js
@@ -1,16 +1,16 @@
 $(() => {
-    const sideMenuWrapper = $('#side_menu');
-    const animateType = "easeOutBounce";
-    sideMenuWrapper.mouseover(()=>{
-        sideMenuWrapper.stop().animate({
+    const sideMenuWrapper = $('#left_side_menu');
+    let animateType = "easeInOutQuint";
+    sideMenuWrapper.mouseover(function(){
+        $(this).stop().animate({
             left: 0,
-        }, 700, animateType);
+        }, 500, animateType);
     });
 
-    sideMenuWrapper.mouseleave(()=>{
-        sideMenuWrapper.stop().animate({
-            left: -380,
-        }, 700, animateType);
+    sideMenuWrapper.mouseleave(function(){
+        $(this).stop().animate({
+            left: -230,
+        }, 500, animateType);
     });
 
     setCurrentTimeInfo();
@@ -29,9 +29,8 @@ function setCurrentTimeInfo(){
     const sec = current.getSeconds();
     
     $('#current_date').text(`${year}년 ${month}월 ${day}일`);
-    $('#current_time').text(`${pad(hour+"",2)}:${pad(min+"",2)}`);
+    $('#current_time').text(`${hour}:${pad(min+"",2)}`);
     $('#current_sec').text(pad(sec+"",2));
     $('#current_time_split').text(current.isPM()?"PM":"AM");
     $('#current_time').css('text-shadow', `0 0 10px ${current.isPM()?"blue":"red"}`);
-    // $('#current_time_split').css('color', `${current.isPM()?"blue":"red"}`);
 }
